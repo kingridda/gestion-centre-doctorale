d24d38d2ce2b8a14cb100615057b81bae6736fa6
Anas-Abou-Ali <anasabouali99@gmail.com>
2020-04-30 23:48:28 +0000

    test1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,22 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+    <style>
+            .footer {
+                  position: fixed;
+                  bottom: 0;
+                  width: 100%;
+                  height: 10rem;            /* Footer height */
+                } 
+            body {
+                    background: url('/img/background1.jpg') no-repeat center center fixed;
+                    -webkit-background-size: cover;
+                    -moz-background-size: cover;
+                    background-size: cover;
+                    -o-background-size: cover;
+                }
+    </style>
+
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
@@ -26,6 +42,28 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
+                <ul class="navbar-nav">               <!-- Navbar tabs-->
+                  <li class="nav-item" role="tablist">
+                    <a class="nav-link" href="/show"  >Profile</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Envoyer</a>
+                    <div class="dropdown-menu">
+                      <a class="dropdown-item" href="/profile/encm">Encadrant</a>
+                      <div class="dropdown-divider"></div>
+                      <a class="dropdown-item" href="/profile/compte_rendu">Compte Rendu</a>
+                    </div>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/messages">Messages</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/forum">Forum</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/profile/dirm">Direction</a>
+                  </li>
+                </ul>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -41,24 +79,24 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Se connecter') }}</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="/salarie">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="/salarie">{{ __("S'inscrire") }}</a>
                                 </li>
                             @endif
                         @else 
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ "Bonjour " .Auth::user()->prenom ." ". Auth::user()->name }} <span class="caret"></span>
+                                    {{ "Bonjour " .Auth::user()->Prenom ." ". Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        {{ __('Se déconnecter') }}
                                     </a>
               
                                 
@@ -73,10 +111,40 @@
                 </div>
             </div>
         </nav>
-
         <main class="py-4">
             @yield('content')
         </main>
     </div>
 </body>
+
+
+
 </html>
+<!-- Footer -->
+    <footer class="page-footer font-small bg-dark text-white footer" >
+    
+      <!-- Footer Links -->
+      <div class="container">
+
+          <!-- Grid column -->
+          <div class="col-md-12 mt-0 py-3 text-center">
+            <p style="line-height: 1.7rem">Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem
+              aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+              explicabo.
+              .</p>
+          </div>
+          <!-- Grid column -->
+
+        </div>
+
+      <!-- Footer Links -->
+
+      <!-- Copyright -->
+      <div class="footer-copyright text-center py-3">© 2020 Copyright:
+        <a href="https://google.com"> GCD.com</a>
+      </div>
+      <!-- Copyright -->
+
+    </footer>
+    <!-- Footer -->
