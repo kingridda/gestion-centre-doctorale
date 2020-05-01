e16f65182a582f32ab60d6da8f579cdf2633b7b5
Anas-Abou-Ali <anasabouali99@gmail.com>
2020-05-01 01:16:00 +0000

    test1

--- resources/views/etudiantV/show_profile.blade.php
+++ resources/views/etudiantV/show_profile.blade.php
@@ -1,7 +1,64 @@
 @extends('layouts.app')
 @section('content')
 	<div class="container">
-		je vois les info de mon profile
-		
+		<div class="row mt-5">
+			<div class="col-lg-4 " >
+				<center>
+				<img src="/avatars/{{$user->avatar}}" style="border-radius:100% ">
+				<div>
+					<a href="/change_avatar" > changer la photo</a>
+				</div>
+			</center>
+
+			</div>
+			<div class="col-lg-3  mt-4 " >
+				<center>
+					<h2>
+						{{$user->name }} 
+						<br>
+						
+						{{$user->prenom}}
+						<br>
+				
+					
+				
+					</h2>
+				</center>
+			</div>
+
+			<div class="col-lg-6 ml-3 mt-4 " >
+				
+				<h2> 
+					Cin  
+					<br>
+				
+					Email  
+					<br>
+				
+					Adresse	 
+					<br>
+				
+					Ville  
+					<br>
+				
+					Encadrant 
+				
+					: {{$user->cin}}
+					<br>
+				
+					: {{$user->email}}
+					<br>
+				
+					: {{$user->adresse}}
+					<br>
+				
+					: {{$user->ville}}
+					<br>
+				
+					: {{$user->encadrant_id}}
+				
+				</h2>
+			</div>
+		</div>
 	</div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,20 +4,49 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>GCD</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <!-- CSS Bootstarp -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        
+        <header class="bgimage">
+            <div class="flex-center position-ref full-height">
+            
+        <!login et inscription >
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/profile') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Se connecter</a>
 
+                        @if (Route::has('register'))
+                            <a href="/salarie">S'inscrire</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+            </div>
+        </header>
+            
         <!-- Styles -->
         <style>
-            html, body {
+        html, body {
                 background-color: #fff;
-                color: #636b6f;
+                color: #000000;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
-                height: 100vh;
+                height: 100%;
                 margin: 0;
+
+                background: url('/img/background1.jpg') no-repeat center center fixed;
+                -webkit-background-size: cover;
+                -moz-background-size: cover;
+                background-size: cover;
+                -o-background-size: cover;
+
             }
 
             .full-height {
@@ -25,7 +54,7 @@
             }
 
             .flex-center {
-                align-items: center;
+                /*align-items: center;*/
                 display: flex;
                 justify-content: center;
             }
@@ -49,9 +78,9 @@
             }
 
             .links > a {
-                color: #636b6f;
+                color: #fff;
                 padding: 0 25px;
-                font-size: 13px;
+                font-size: 20px;
                 font-weight: 600;
                 letter-spacing: .1rem;
                 text-decoration: none;
@@ -59,32 +88,118 @@
             }
 
             .m-b-md {
-                margin-bottom: 30px;
+                margin-top: 50px;
+                font-family: cursive;
+                font-size: 120px;
             }
+            .bgimage {
+                background-image: url("img/background.jpg");
+                background-position: center center;
+                background-size: cover;
+                height:300px;
+            }
+            .footer {
+                  position: relative;
+                  bottom: 0;
+                  width: 100%;
+                  height: 10rem;            /* Footer height */
+                }            
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/profile') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="/salarie">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Guest page
-                </div>
-
+        <div class="container">
+           <center> <h1>Nos filiere</h1></center>
+          <div class="row">
+            <div class="col-lg-4 mt-5 ">
+                <center>
+                <img src="img/logiciel.png" style="border-radius:100%" width="300px" >
+                
+                    <h3>Génie logiciel</h3> 
+                <br>
+              <p> je suis tres fiere de se travail ....asdfghjhg fdsa  aaaaa aaaaaaa aa aaaaaa aaaa aaa  aaaaa a aaa aaaa aa aaaa a aaaaa  aaaaaa aa aaaaaa aaa a aaaaa aaa aaaaaa aa aaaaaa aa aa aaaa a aaaaaaa a aa aaaa aa aa aaaa aa aaaa aa aaa aa aa aaaaa aaa aaaa aaa aaaaaaa a a aaaaaaa aa aaaa aaa</p>
+            </center>
             </div>
+            <div class="col-lg-4 mt-5">
+                <center>
+                <img src="img/info_decision.png" style="border-radius:100%" width="300px">
+            
+                    <h3>Informatique décisionnelle</h3>
+                <br>
+              <p> je suis tres fiere de se travail ....asdfghjhg fdsa  aaaaa aaaaaaa aa aaaaaa aaaa aaa  aaaaa a aaa aaaa aa aaaa a aaaaa  aaaaaa aa aaaaaa aaa a aaaaa aaa aaaaaa aa aaaaaa aa aa aaaa a aaaaaaa a aa aaaa aa aa aaaa aa aaaa aa aaa aa aa aaaaa aaa aaaa aaa aaaaaaa a a aaaaaaa aa aaaa aaa</p>
+            </center>
+            </div>
+            <div class="col-lg-4 mt-5">
+                <center>
+                <img src="img/reseau.png" style="border-radius:100%" width="300px">
+    
+                    <h3>Réseaux et télécommunications</h3>
+                <br>
+              <p> nssar maxi rajl et merci....asdfghjhg fdsa  aaaaa aaaaaaa aa aaaaaa aaaa aaa  aaaaa a aaa aaaa aa aaaa a aaaaa  aaaaaa aa aaaaaa aaa a aaaaa aaa aaaaaa aa aaaaaa aa aa aaaa a aaaaaaa a aa aaaa aa aa aaaa aa aaaa aa aaa aa aa aaaaa aaa aaaa aaa aaaaaaa a a aaaaaaa aa aaaa aaa</p>
+            </center>   
+            </div>
+          </div>
+            
+            <div class="row">
+            <div class="col-lg-4 mt-5 ">
+                <center>
+                <img src="img/elec.png" style="border-radius:100%" width="300px" >
+                
+                    <h3>Génie électrique</h3> 
+                <br>
+              <p> je suis tres fiere de se travail ....asdfghjhg fdsa  aaaaa aaaaaaa aa aaaaaa aaaa aaa  aaaaa a aaa aaaa aa aaaa a aaaaa  aaaaaa aa aaaaaa aaa a aaaaa aaa aaaaaa aa aaaaaa aa aa aaaa a aaaaaaa a aa aaaa aa aa aaaa aa aaaa aa aaa aa aa aaaaa aaa aaaa aaa aaaaaaa a a aaaaaaa aa aaaa aaa</p>
+            </center>
+            </div>
+            <div class="col-lg-4 mt-5">
+                <center>
+                <img src="img/genie_meca.png" style="border-radius:100%" width="300px">
+            
+                    <h3>Génie mécanique</h3>
+                <br>
+              <p> je suis tres fiere de se travail ....asdfghjhg fdsa  aaaaa aaaaaaa aa aaaaaa aaaa aaa  aaaaa a aaa aaaa aa aaaa a aaaaa  aaaaaa aa aaaaaa aaa a aaaaa aaa aaaaaa aa aaaaaa aa aa aaaa a aaaaaaa a aa aaaa aa aa aaaa aa aaaa aa aaa aa aa aaaaa aaa aaaa aaa aaaaaaa a a aaaaaaa aa aaaa aaa</p>
+            </center>
+            </div>
+            <div class="col-lg-4 mt-5">
+                <center>
+                <img src="img/ener_env.png" style="border-radius:100%" width="300px">
+    
+                    <h3>Génie Energétique et Environnement</h3>
+                <br>   
+              <p> nssar maxi rajl et merci....asdfghjhg fdsa  aaaaa aaaaaaa aa aaaaaa aaaa aaa  aaaaa a aaa aaaa aa aaaa a aaaaa  aaaaaa aa aaaaaa aaa a aaaaa aaa aaaaaa aa aaaaaa aa aa aaaa a aaaaaaa a aa aaaa aa aa aaaa aa aaaa aa aaa aa aa aaaaa aaa aaaa aaa aaaaaaa a a aaaaaaa aa aaaa aaa</p>
+            </center>   
+            </div>
+          </div>
         </div>
+        </div>
+        <br> <br> 
     </body>
-</html>
+        <!-- Footer -->
+    <footer class="page-footer font-small bg-dark text-white footer" >
+    
+      <!-- Footer Links -->
+      <div class="container">
+
+          <!-- Grid column -->
+          <div class="col-md-12 mt-0 py-3 text-center">
+            <p style="line-height: 1.7rem">Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem
+              aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+              explicabo.
+              .</p>
+          </div>
+          <!-- Grid column -->
+
+        </div>
+
+      <!-- Footer Links -->
+
+      <!-- Copyright -->
+      <div class="footer-copyright text-center py-3">© 2020 Copyright:
+        <a href="https://google.com/"> GCD.com</a>
+      </div>
+      <!-- Copyright -->
+
+    </footer>
+    <!-- Footer -->
+
+    </html>
+
